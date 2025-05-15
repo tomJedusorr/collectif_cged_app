7bec6b9b0a50e6c87a47554919a63e28d39605a5
tomJedusorr <raphael.dahomay@gmail.com>
2025-05-15 18:09:59 +0200

    update settings.py

--- dad_cged_webapp/settings.py
+++ dad_cged_webapp/settings.py
@@ -53,10 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'dad_cged_webapp.urls'
 
