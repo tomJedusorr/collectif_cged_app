e4acbb336e5e44ed373de6f1a68dd5749ed78349
tomJedusorr <raphael.dahomay@gmail.com>
2025-05-17 12:46:07 +0200

    changes

--- dad_cged_webapp/settings.py
+++ dad_cged_webapp/settings.py
@@ -18,7 +18,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'insecure-default-key-change-this')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
